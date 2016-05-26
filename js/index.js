aef38b390a2f9b5a11d8acfa3a97b40809a0716c
juan-imares <1032510958@qq.com>
2016-05-26 16:28:16 +0800

    完成尾部

--- js/index.js
+++ js/index.js
@@ -26,3 +26,18 @@ $("#def-nav .drop").mouseout(function(){
         "display":"none"
     });
 });
+/*底部 li 定时滚动*/
+var _this = $(".link-slide").first();
+var lineH=_this.find("li:first").height(); //获取行高
+var upHeight=0-1*lineH; //滚动高度
+function scrollGun(){
+    _this.animate({
+        marginTop:upHeight
+    },500,function(){
+        _this.find("li:first").appendTo(_this);  //目标点匹配一个元素是移动 目标点匹配多个元素原元素保留 复制到各匹配目标
+        _this.css({marginTop:0});
+    });
+}
+setInterval(function(){
+    scrollGun();
+},2000);
