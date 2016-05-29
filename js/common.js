9d1e92410c571ae6b782138a5a6a645d01fd1776
juan-imares <1032510958@qq.com>
2016-05-30 01:24:24 +0800

    周日 首页基本完成 列表页小部分

--- js/common.js
+++ js/common.js
@@ -13,8 +13,50 @@ function loadHtml(url,target){
 $(window).scroll(function(){
 	var scTop = $(window).scrollTop();
 	if (scTop>=$(".header_bg").height()) {
-		$("#nav").addClass("fixed");
+		$("#nav").addClass("fixed");          //导航栏
+		$("#go2top").css("display","block");  //返回顶部
+		$("#loutiwrap").css("display","block"); //楼梯
 	}else{
 		$("#nav").removeClass("fixed");
+		$("#go2top").css("display","none");
+		$("#loutiwrap").css("display","none");
 	}
-})
+});
+/*返回顶部*/
+$("#go2top").on("click",function(){
+	$("html,body").animate({scrollTop:0},800);
+});
+/*侧边栏 滑出*/
+document.onmousemove = function(e){
+	x = e.clientX;
+	if(x>1310){
+		console.log(1);
+		$(".relative-div .side-other").animate({"left":"0px"});
+	}/*else if(x<1310){
+		$(".relative-div .side-other").animate({"left":"38px"});
+	}*/
+	
+}
+
+/*楼梯*/
+var isClick = false;
+$(".louti li").on("click", function () {
+    isClick = true;
+    //siblings()除了自己以外的其他的兄弟节点
+    $(this).find("span").addClass("active").parent().siblings().find("span").removeClass("active");
+    var docTop = $(".module").eq($(this).index()).offset().top;
+    $("body,html").stop().animate({"scrollTop":docTop-200+ 'px'},1000, function () {
+        isClick = false;
+    });
+});
+$(window).on("scroll",function(){
+    var scrollTop = $(window).scrollTop();
+    if(isClick == false){
+        $(".module").each(function(){
+            if(scrollTop >= $(this).offset().top-($(this).outerHeight())/2){
+                $(".louti li").eq($(this).index()).find("span").addClass("active").parent().siblings().find("span").removeClass("active");
+            }
+        });
+    }
+
+});
--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-/*--------------------------------ͷ�� ���ؿͻ��� ����------------------------*/
-$("#com-topbar .drop").mouseover(function(){
+/*--------------------------------下载app 下拉------------------------*/
+;$("#com-topbar .drop").mouseover(function(){
     $("#com-topbar .down").css({
         "display":"block",
         "top":"31px",
@@ -11,7 +11,7 @@ $("#com-topbar  .drop").mouseout(function(){
         "display":"none"
     });
 });
-/*------------------------------------���� ���� ����-------------------------------*/
+/*------------------------------------导航栏 更多 下拉-------------------------------*/
 $("#def-nav .drop").mouseover(function(){
     $("#def-nav .down").css({
         "display":"block",
@@ -24,24 +24,24 @@ $("#def-nav .drop").mouseout(function(){
         "display":"none"
     });
 });
-/*----------------------------------�ײ� li ��ʱ����------------------------------*/
+/*-------------------底部滚动-------锟阶诧拷 li 锟斤拷时锟斤拷锟斤拷--------------*/
 var _this = $(".link-slide").first();
-var lineH=_this.find("li:first").height(); //��ȡ�и�
-var upHeight=0-1*lineH; //�����߶�
+var lineH=_this.find("li:first").height(); //锟斤拷取锟叫革拷
+var upHeight=0-1*lineH; //锟斤拷锟斤拷锟竭讹拷
 function scrollGun(){
     _this.animate({
         marginTop:upHeight
     },500,function(){
-        _this.find("li:first").appendTo(_this);  //Ŀ���ƥ��һ��Ԫ�����ƶ� Ŀ���ƥ����Ԫ��ԭԪ�ر��� ���Ƶ���ƥ��Ŀ��
+        _this.find("li:first").appendTo(_this);  //目锟斤拷锟狡ワ拷锟揭伙拷锟皆�拷锟斤拷锟斤拷贫锟�目锟斤拷锟狡ワ拷锟斤拷锟皆�拷锟皆��拷乇锟斤拷锟�锟斤拷锟狡碉拷锟斤拷匹锟斤拷目锟斤拷
         _this.css({marginTop:0});
     });
 }
-/*------------------------------------��ʱ��--------------------------------------*/
+/*----------底部 滚动---------*/
 setInterval(function(){
-    scrollGun();    //�ײ�����
+    scrollGun();    //滚动
 },3000);
 
-/*-------------------------------�����˵� ��ʽ����--------------------------------*/
+/*-------------------------------二级菜单--------------------------------*/
 $(".sec_menu").find("li").on("mouseover",function(){
     var pre = $($(".sec_menu .list")[0]).prev();
     pre.css({"border":"none"});
@@ -55,8 +55,8 @@ $(".sec_menu").find("li").on("mouseout",function(){
     $(".sec_menu .list").removeClass("active"), $(this).removeClass("active"), $(this).children(".thir_menu").hide();
 });
 
-/*-------------------------------�ֲ�ͼ---------------------------------*/
-/*�����ֲ�ͼƬ*/
+/*-------------------------------轮播图---------------------------------*/
+/*引入轮播图片*/
 $.ajax({
     type:"get",
     url:"../data/banner.json",
@@ -85,7 +85,7 @@ function bannerMove(){
     $(".banner li").filter(":visible").fadeOut(300).parent().children().eq(n).fadeIn(500);
     $(".bnr_btn a").eq(n).addClass("on").siblings().removeClass("on");
 }
-//��������Ƴ�
+//轮播图 鼠标移入暂停 移出继续
 $(".banner_wrap").hover(function(){
     clearInterval(timer);
     $(".bnr_btn_lr").css("display","block");
@@ -93,21 +93,21 @@ $(".banner_wrap").hover(function(){
     timer = setInterval("bannerMove()",3000);
     $(".bnr_btn_lr").css("display","none");
 });
-//����ť
+//轮播图 左侧按钮
 $(".bnr_btn_l").click(function() {
     $(".banner li").eq(n).fadeOut(300);
     $(".banner li").eq(n-1).fadeIn(500);
     $(".bnr_btn a").eq(n-1).addClass("on").siblings().removeClass("on");
     n = n-1;
 });
-//����ť
+//轮播图 右侧按钮
 $(".bnr_btn_r").click(function(){
     $(".banner li").eq(n).fadeOut(300);
     $(".banner li").eq(n+1).fadeIn(500);
     $(".bnr_btn a").eq(n+1).addClass("on").siblings().removeClass("on");
     n = n+1;
 });
-/*���� fashion*/
+/*主体内容 fashion*/
 $.ajax({
     type:"get",
     url:"../data/fashion.json",
@@ -134,7 +134,7 @@ $.ajax({
             j++;
         }
     }
-})
+});
 /*beauty*/
 $.ajax({
     type:"get",
@@ -147,7 +147,7 @@ $.ajax({
             if(j==3){j=0;}
             $(".beauty .cont .item .pic").eq(j).append("<a href="+msg[i].url+">"+"<img src="+msg[i].src+"/>"+"</a>");
             $(".beauty .cont .item .desc .two_price").eq(j).append("<span class='price'>"+msg[i].price+"</span>");
-            $(".beauty .cont .item .desc .two_price").eq(j).append("<span class='like'>"+msg[i].oriprice+"</span>");
+            $(".beauty .cont .item .desc .two_price").eq(j).append("<span class='ori_price'>"+msg[i].oriprice+"</span>");
             $(".beauty .cont .item .desc .tit").eq(j).append("<a href="+msg[i].url+">"+msg[i].name+"</a>");
             $(".beauty .cont .item .desc .subtit").eq(j).append(msg[i].desc);
             $(".beauty .cont .item .tag").eq(j).append("<a href="+msg[i].url+">"+msg[i].word1+"</a>");
@@ -156,7 +156,107 @@ $.ajax({
             j++;
         }
     }
-})
+});
+/*hi范儿 引入图片*/
+$.ajax({
+    type:"get",
+    url:"../data/hifan.json",
+    contentType : "application/json",
+    dataType:"json",
+    "success": function(msg){
+        var j = 0;
+        for(var i in msg){
+            if(j==12){j=0;}
+            	$(".hifan .cont .item").eq(j).append("<a href="+msg[i].url+">"+"<img src="+msg[i].src+"/>"+"</a>");
+            j++;
+        }
+    }
+});
+/*hi范儿 选项卡*/
+$(".hifan .cont ul li").on("click",function(){
+	$(this).addClass("active").siblings().removeClass("active");
+	$(".hifan .tab-item").eq($(this).index()).addClass("active").siblings().removeClass("active");
+});
+/*好店推荐 导入数据*/
+$.ajax({
+    type:"get",
+    url:"../data/recoShop.json",
+    contentType : "application/json",
+    dataType:"json",
+    "success": function(msg){
+        var j = 0;
+        for(var i in msg){
+            if(j==15){j=0;}
+            $(".recommend_shop .cont .tab-inner .item").eq(j).prepend("<img src="+msg[i].src+"/>");
+            $(".recommend_shop .cont .item .desc .tit").eq(j).append("<a href="+msg[i].url+">"+msg[i].tit+"</a>");
+            $(".recommend_shop .cont .item .desc .subtit").eq(j).append(msg[i].subtit);
+            $(".recommend_shop .cont .item .desc .saleScore .salenum").eq(j).append(msg[i].salenum);
+            $(".recommend_shop .cont .item .desc .saleScore .last").eq(j).append(msg[i].score);
+            $(".recommend_shop .cont .item .mask .go").eq(j).append("<a href="+msg[i].url+">"+msg[i].go+"</a>");
+            j++;
+        }
+    }
+});
+/*好店推荐 选项卡*/
+$(".recommend_shop .cont .tab div").on("click",function(){
+	$(this).addClass("active").siblings().removeClass("active");
+	$(".recommend_shop .tab-item").eq($(this).index()).addClass("active").siblings().removeClass("active");
+});
+/*好店推荐 鼠标移入事件mask显示*/
+$(".recommend_shop .cont .item").hover(function(){
+	$(".mask", this).show();
+},function(){
+	$(".mask", this).hide();
+});
+$(".unadd").hover(function(){
+	$(this).html("取消关注");
+},function(){
+	$(this).html("已关注");
+});
 
+/*猜你喜欢*/
+$.ajax({
+    type:"get",
+    url:"../data/guessLike.json",
+    contentType : "application/json",
+    dataType:"json",
+    "success": function(msg){
+        var j = 0;
+        for(var i in msg){
+            if(j==10){j=0;}
+            $(".guess_like .cont .item .pic").eq(j).append("<a href="+msg[i].url+">"+"<img src="+msg[i].src+"/>"+"</a>");
+            $(".guess_like .cont .item .desc .price_like").eq(j).append("<span class='price'>"+msg[i].price+"</span>");
+            $(".guess_like .cont .item .desc .price_like").eq(j).append("<span class='like'>"+msg[i].likeNum+"</span>");
+            $(".guess_like .cont .item .desc .tit").eq(j).append("<a href="+msg[i].url+">"+msg[i].desc+"</a>");
+            j++;
+        }
+    }
+});
+var urlIndex = 2;
+$(".guess_like .tit .change").on("click",function(){
+	//if(urlIndex==1){urlIndex=2;}
+	var url_like = "../data/guessLike"+urlIndex+".json";
+	//urlIndex--;
+	$.ajax({
+    type:"get",
+    url:url_like,
+    contentType : "application/json",
+    dataType:"json",
+    "success": function(msg){
+        var j = 0;
+        for(var i in msg){
+            if(j==10){j=0;}
+            $(".guess_like .cont .item .pic").eq(j).empty();
+            $(".guess_like .cont .item .pic").eq(j).append("<a href="+msg[i].url+">"+"<img src="+msg[i].src+"/>"+"</a>");
+            $(".guess_like .cont .item .desc .price_like").eq(j).empty();
+            $(".guess_like .cont .item .desc .price_like").eq(j).append("<span class='price'>"+msg[i].price+"</span>");
+            $(".guess_like .cont .item .desc .price_like").eq(j).append("<span class='like'>"+msg[i].likeNum+"</span>");
+            $(".guess_like .cont .item .desc .tit").eq(j).empty();
+            $(".guess_like .cont .item .desc .tit").eq(j).append("<a href="+msg[i].url+">"+msg[i].desc+"</a>");
+            j++;
+        }
+    }
+});
+})
 
 
